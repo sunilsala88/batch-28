b290f393b1602900a382a81b65a5e2fba590d1a3
sunilsala88 <sunilsala88@gmail.com>
2024-09-29 20:18:41 +0530

    ff

--- datastructure/dictionary1.py
+++ datastructure/dictionary1.py
@@ -1,6 +1,10 @@
-stock_prices={'amzn':500,'tsla':900,'goog':790,99:100}
+stock_prices={'amzn':500,'tsla':900,'goog':900,99:100}
 print(stock_prices)
 
+#accessing new way
+print(stock_prices.get('nvda'))
+
+#accessing old way
 print(stock_prices['tsla'])
 print(stock_prices[99])
 
@@ -13,10 +17,14 @@ stock_prices['banknifty']=590
 print(stock_prices)
 
 #deleting a key value pair
-stock_prices.pop('goog')
+stock_prices.pop(99)
 print(stock_prices)
 
 #old way
 del stock_prices['amzn']
 print(stock_prices)
 
+#values keys items
+print(list(stock_prices.keys()))
+print(list(stock_prices.values()))
+print(list(stock_prices.items()))
