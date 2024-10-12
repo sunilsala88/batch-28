e8e8e95b4b62fd5d09b4573c6ca245a1dad37155
sunilsala88 <sunilsala88@gmail.com>
2024-10-12 19:32:27 +0530

    oct12_2

--- 4.loops/loops1.py
+++ 4.loops/loops1.py
@@ -1,7 +1,46 @@
-l1=[55,66,77]
+l1=[5,6,7]
 
-#type 1
+#type 1->go through each elem of list
 total=0
 for elem in l1:
     total=total+elem
+print(total)
+avg=total/len(l1)
+print(avg)
+
+#type 2-> run a piece of code for n times
+a='python'
+l2=[0,1,2,3,4,5,6,7,8,9]
+l3=list(range(100))
+print(l3)
+for i in range(100):
+    print(a)
+
+#type 3->go through each elem of list using its index
+prices=[666,777,888,33,99,78,99]
+indexes=[0,1,2,3,4]
+indexes=range(len(prices))
+for i in range(len(prices)):
+    print(prices[i])
+
+
+#problem 1
+r1=list(range(10))
+print(r1)
+
+r2=list(range(1,101))
+print(r2)
+
+r3=list(range(0,101,10))
+print(r3)
+
+# for i in r1:
+#     if i%2==0:
+#         print(i)
+
+even=list(range(0,101,2))
+print(even)
+total=0
+for i in even:
+    total=total+i
 print(total)
