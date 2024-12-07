e586870baaae5ee39eeae1691afc33a6e43adf61
sunilsala88 <sunilsala88@gmail.com>
2024-12-07 19:09:36 +0530

    dec-7-2

--- 13_fundamental/fund2.py
+++ 13_fundamental/fund2.py
@@ -7,10 +7,24 @@
 
 
 
-from finvizfinance.quote import finvizfinance
+# from finvizfinance.quote import finvizfinance
 
-stock = finvizfinance('NVDA')
+# stock = finvizfinance('TSLA')
 
-stock_fundament = stock.ticker_fundament()
+# stock_fundament = stock.ticker_fundament()
 
-print(stock_fundament)
+# print(stock_fundament)
+
+# print(stock.ticker_news())
+
+# inside_trader_df = stock.ticker_inside_trader()
+# print(inside_trader_df)
+
+
+from finvizfinance.screener.overview import Overview
+
+foverview = Overview()
+filters_dict = {'Index':'S&P 500'}
+foverview.set_filter(filters_dict=filters_dict)
+df = foverview.screener_view()
+print(df)
---
+++ 14_technical/tech1.py
@@ -0,0 +1,41 @@
+#yahoo finance
+#yfinance==0.2.37
+import yfinance as yf
+import pandas as pd
+import pandas_ta as ta
+# #talib
+# #pandasta
+data=yf.download(tickers='^NSEI',period='1y')
+print(data)
+
+# col=[]
+# for c in data.columns:
+#     col.append(c[0])
+# data.columns=col
+
+# data.columns=[c[0] for c in data.columns]
+# print(data)
+print(ta.sma(data['Close']))
+
+#installing talib
+#pip install "/Users/algotrading2024/batch 28/ta_lib-0.5.1-cp312-cp312-win_amd64.whl"
+# import talib 
+# print(talib.get_functions())
+
+
+
+#mac os
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#add path
+#source ~/.zshrc
+#source ~/.bash_profile
+#brew --version
+#brew install ta-lib
+#pip3 install TA-Lib
+
+
+#pandas-ta
+#pip install pandas-ta
+# error pkg_resources
+#  pip install setuptools
+#pip install numpy==1.26.4 
