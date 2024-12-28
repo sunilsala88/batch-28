31bad45affd6d60dc4c2c0ecf3dbeba2f52c8411
sunilsala88 <sunilsala88@gmail.com>
2024-12-28 19:00:35 +0530

    dec-28

--- 16_broker_basics/basics1.py
+++ 16_broker_basics/basics1.py
@@ -1,22 +1,22 @@
-from alpaca.data.historical import CryptoHistoricalDataClient
-from alpaca.data.requests import CryptoBarsRequest
-from alpaca.data.timeframe import TimeFrame
-from datetime import datetime
+# from alpaca.data.historical import CryptoHistoricalDataClient
+# from alpaca.data.requests import CryptoBarsRequest
+# from alpaca.data.timeframe import TimeFrame
+# from datetime import datetime
 
-# no keys required for crypto data
-client = CryptoHistoricalDataClient()
+# # no keys required for crypto data
+# client = CryptoHistoricalDataClient()
 
-request_params = CryptoBarsRequest(
-                        symbol_or_symbols=["BTC/USD", "ETH/USD"],
-                        timeframe=TimeFrame.Day,
-                        start=datetime(2022, 7, 1),
-                        end=datetime(2022, 9, 1)
-                 )
+# request_params = CryptoBarsRequest(
+#                         symbol_or_symbols=["BTC/USD", "ETH/USD"],
+#                         timeframe=TimeFrame.Day,
+#                         start=datetime(2022, 7, 1),
+#                         end=datetime(2022, 9, 1)
+#                  )
 
-bars = client.get_crypto_bars(request_params)
+# bars = client.get_crypto_bars(request_params)
 
-# convert to dataframe
-print(bars.df)
+# # convert to dataframe
+# print(bars.df)
 
 
 from credentials import api_key,secret_key
